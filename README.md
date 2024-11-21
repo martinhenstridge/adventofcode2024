69fd0c916d44988deb1b848096293fe7e556e3db
Martin Henstridge <mchenstridge@gmail.com>
2024-11-21 16:01:33 +0000

    Initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# adventofcode2024
+
+Run solutions as follows:
+
+```
+python3 -m aoc [DAY]
+```
+
+Omit the `DAY` argument altogether to run all solutions sequentially, or specify `DAY` as a two-digit number (e.g. `01`) to run only the solution for that day's problem.
---
+++ aoc/__main__.py
@@ -0,0 +1,29 @@
+import sys
+import time
+import pathlib
+
+from . import DAYS
+
+_INPUTS = pathlib.Path(__file__).parents[1] / "inputs"
+
+
+def run(day, solver, solutions):
+    start = time.perf_counter()
+    with open(_INPUTS / day) as f:
+        data = f.read()
+    results = solver.run(data)
+    end = time.perf_counter()
+
+    print()
+    for idx, result in enumerate(results):
+        print(f"[{day}/{idx+1}] {result}")
+    print(f"{1000 * (end - start):.3f}ms")
+    assert results == solutions
+
+
+if len(sys.argv) > 1:
+    day = sys.argv[1]
+    run(day, *DAYS[day])
+else:
+    for day in DAYS:
+        run(day, *DAYS[day])
