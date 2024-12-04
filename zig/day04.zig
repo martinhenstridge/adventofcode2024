f2b516755e658306b9bf483f368f03c723a05b10
Martin Henstridge <mchenstridge@gmail.com>
2024-12-04 23:43:09 +0000

    day 4 zig, tidying

--- zig/day04.zig
+++ zig/day04.zig
@@ -2,18 +2,39 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 
-const Position = struct { row: i16, col: i16 };
 const Direction = struct { dr: i8, dc: i8 };
+const Position = struct {
+    row: i16,
+    col: i16,
+
+    fn add(self: Position, dir: Direction) Position {
+        return .{
+            .row = self.row + dir.dr,
+            .col = self.col + dir.dc,
+        };
+    }
+
+    fn sub(self: Position, dir: Direction) Position {
+        return .{
+            .row = self.row - dir.dr,
+            .col = self.col - dir.dc,
+        };
+    }
+};
+
+const U = Direction{ .dr = -1, .dc = 0 };
+const R = Direction{ .dr = 0, .dc = 1 };
+const D = Direction{ .dr = 1, .dc = 0 };
+const L = Direction{ .dr = 0, .dc = -1 };
+
+const UR = Direction{ .dr = -1, .dc = 1 };
+const DR = Direction{ .dr = 1, .dc = 1 };
+const DL = Direction{ .dr = 1, .dc = -1 };
+const UL = Direction{ .dr = -1, .dc = -1 };
 
 const DIRECTIONS = [_]Direction{
-    .{ .dr = -1, .dc = 0 },
-    .{ .dr = 0, .dc = 1 },
-    .{ .dr = 1, .dc = 0 },
-    .{ .dr = 0, .dc = -1 },
-    .{ .dr = -1, .dc = 1 },
-    .{ .dr = 1, .dc = 1 },
-    .{ .dr = 1, .dc = -1 },
-    .{ .dr = -1, .dc = -1 },
+    U,  D,  L,  R,
+    UR, DR, DL, UL,
 };
 
 fn extract_grid(input: []const u8, grid: *ArrayList([]const u8)) !void {
@@ -23,35 +44,37 @@ fn extract_grid(input: []const u8, grid: *ArrayList([]const u8)) !void {
     }
 }
 
+fn not_match(grid: [][]const u8, p: Position, target: u8) bool {
+    return grid[@intCast(p.row)][@intCast(p.col)] != target;
+}
+
 fn mas(grid: [][]const u8, start: Position, dir: Direction) bool {
-    var r = start.row;
-    var c = start.col;
+    var p = start;
 
     for ("MAS") |target| {
-        if (r < 0 or r >= grid.len) {
+        if (p.row < 0 or p.row >= grid.len) {
             return false;
         }
-        if (c < 0 or c >= grid[@intCast(r)].len) {
+        if (p.col < 0 or p.col >= grid.len) {
             return false;
         }
-        if (grid[@intCast(r)][@intCast(c)] != target) {
+        if (not_match(grid, p, target)) {
             return false;
         }
-        r += dir.dr;
-        c += dir.dc;
+        p = p.add(dir);
     }
 
     return true;
 }
 
 fn xmas_count(grid: [][]const u8, p: Position) u32 {
-    if (grid[@intCast(p.row)][@intCast(p.col)] != 'X') {
+    if (not_match(grid, p, 'X')) {
         return 0;
     }
 
     var count: u32 = 0;
     for (DIRECTIONS) |dir| {
-        if (mas(grid, .{ .row = p.row + dir.dr, .col = p.col + dir.dc }, dir)) {
+        if (mas(grid, p.add(dir), dir)) {
             count += 1;
         }
     }
@@ -59,21 +82,18 @@ fn xmas_count(grid: [][]const u8, p: Position) u32 {
 }
 
 fn x_mas_count(grid: [][]const u8, p: Position) u32 {
-    if (grid[@intCast(p.row)][@intCast(p.col)] != 'A') {
+    // All X-MAS instances have an A at the centre
+    if (not_match(grid, p, 'A')) {
         return 0;
     }
 
     // Check for \-oriented MAS
-    if (!(mas(grid, .{ .row = p.row - 1, .col = p.col - 1 }, .{ .dr = 1, .dc = 1 }) or
-        mas(grid, .{ .row = p.row + 1, .col = p.col + 1 }, .{ .dr = -1, .dc = -1 })))
-    {
+    if (!(mas(grid, p.sub(DR), DR) or mas(grid, p.sub(UL), UL))) {
         return 0;
     }
 
     // Check for /-oriented MAS
-    if (!(mas(grid, .{ .row = p.row - 1, .col = p.col + 1 }, .{ .dr = 1, .dc = -1 }) or
-        mas(grid, .{ .row = p.row + 1, .col = p.col - 1 }, .{ .dr = -1, .dc = 1 })))
-    {
+    if (!(mas(grid, p.sub(DL), DL) or mas(grid, p.sub(UR), UR))) {
         return 0;
     }
 
@@ -89,7 +109,7 @@ pub fn main(allocator: Allocator, input: []const u8) !void {
 
     try extract_grid(input, &grid);
     for (0..grid.items.len) |r| {
-        for (0..grid.items[r].len) |c| {
+        for (0..grid.items.len) |c| {
             const p = Position{ .row = @intCast(r), .col = @intCast(c) };
             count1 += xmas_count(grid.items, p);
             count2 += x_mas_count(grid.items, p);
