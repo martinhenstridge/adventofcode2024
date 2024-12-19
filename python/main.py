a9e8da92bb9a6b7f013ab601086d7d9694d1cbdc
Martin Henstridge <mchenstridge@gmail.com>
2024-12-19 22:57:10 +0000

    less obtuse python main

--- python/main.py
+++ python/main.py
@@ -3,23 +3,24 @@ import pathlib
 import sys
 import time
 
-PROBLEMS = [(f"inputs/{day:02}", f"day{day:02}") for day in range(1, 20)]
 
-
-days_to_run = {int(arg) for arg in sys.argv[1:]}
-
-
-for day, (input_path, solver_module) in enumerate(PROBLEMS, 1):
-    if days_to_run and day not in days_to_run:
-        continue
-
-    input_text = pathlib.Path(input_path).read_text()
-    solver = importlib.import_module(solver_module)
+def run(day: int) -> None:
+    solver = importlib.import_module(f"day{day:02}")
+    input_text = pathlib.Path(f"inputs/{day:02}").read_text()
 
     start = time.perf_counter_ns()
     solution = solver.run(input_text)
     end = time.perf_counter_ns()
 
-    print(f"{day:>2}-1 :: {solution[0]}")
-    print(f"{day:>2}-2 :: {solution[1]}")
-    print(f"time :: {round((end - start) / 1000):_} \u03bcs\n")
+    print(f"{day:02} ({round((end - start) / 1000):_} \u03bcs)")
+    print(f":: {solution[0]}")
+    print(f":: {solution[1]}")
+    print()
+
+
+if __name__ == "__main__":
+    days_to_run = {int(arg) for arg in sys.argv[1:]}
+    for day in range(1, 20):
+        if days_to_run and day not in days_to_run:
+            continue
+        run(day)
